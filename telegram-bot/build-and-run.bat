bc3c16bd85ce0ac8c780899c3dba15f15214fce0
Marte Valerio Falcone <marte.vfalcone@gmail.ocm>
2020-07-23 16:54:19 +0200

    Added detaching from docker process in Telegram bot

--- telegram-bot/build-and-run.bat
+++ telegram-bot/build-and-run.bat
@@ -1,3 +1,3 @@
 SET ALARM_CONTAINER_TAG=alarm-bot-telegram
 docker build -t %ALARM_CONTAINER_TAG% .
-docker run %ALARM_CONTAINER_TAG%
+docker run -d %ALARM_CONTAINER_TAG%
--- telegram-bot/build-and-run.sh
+++ telegram-bot/build-and-run.sh
@@ -1,3 +1,3 @@
 ALARM_CONTAINER_TAG=alarm-bot-telegram
 docker build -t $ALARM_CONTAINER_TAG .
-docker run $ALARM_CONTAINER_TAG
+docker run -d $ALARM_CONTAINER_TAG
